c66d13049d357aef65794242c8a42258e4210b5b
Haider Bastian <bastian.haider@gmx.at>
2018-09-27 11:34:27 +0200

    first steps

---
+++ calculator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  printf("Add(1)\n");
+  printf("Subtract(2)\n");
+  printf("Multiply(3)\n");
+  printf("Divide\n");
+  printf("Stop programm(-1)\n");
+  printf("Enter your choice: \n");
+  int input;
+  scanf("%d\n,",&input);
+  //switch (input) {
+  //  case 1:printf("Please Enter the first Operant: \n", );
+//  }
+  return 0;
+}
